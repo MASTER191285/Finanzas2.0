7266de17db3b24d2b3adaf337960efb129b261d9
Yuri <master191285@hotmail.com>
2018-12-04 16:28:47 -0300

    Script SQL
    Update con SP

--- db/finanzas.sql
+++ db/finanzas.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1:3306
--- Tiempo de generación: 27-11-2018 a las 20:21:48
+-- Tiempo de generación: 04-12-2018 a las 19:27:54
 -- Versión del servidor: 5.7.23
 -- Versión de PHP: 7.2.10
 
@@ -59,27 +59,33 @@ END$$
 
 DROP PROCEDURE IF EXISTS `GET_TOTALESV2`$$
 CREATE DEFINER=`root`@`localhost` PROCEDURE `GET_TOTALESV2` (IN `Id_User` INT(11))  BEGIN
+DECLARE FECHA_INI DATETIME;
+SET FECHA_INI= (SELECT DATE(NOW()) - INTERVAL DAY(NOW()) DAY + INTERVAL 1 DAY);
 SELECT 	  (
       	SELECT SUM(G2.MONTO)
         FROM gastos AS G2
         WHERE G2.id_usuario = G.id_usuario
+        AND G2.fecha BETWEEN FECHA_INI AND CURDATE()
       ) AS GASTOS,
 	 (
       	SELECT SUM(I2.MONTO)
         FROM ingresos AS I2
         WHERE I2.id_usuario = G.id_usuario
+        AND I2.fecha BETWEEN FECHA_INI AND SYSDATE()
       ) AS INGRESOS,
       (
         (
       		SELECT SUM(I2.MONTO)
         	FROM ingresos AS I2
         	WHERE I2.id_usuario = G.id_usuario
+        	AND I2.fecha BETWEEN FECHA_INI AND SYSDATE()
       	) 
       	-
       	(
       		SELECT SUM(G2.MONTO)
         	FROM gastos AS G2
         	WHERE G2.id_usuario = G.id_usuario
+        	AND G2.fecha BETWEEN FECHA_INI AND CURDATE()
       	)
       )AS TOTAL
       
@@ -87,6 +93,43 @@ FROM GASTOS AS G
 WHERE G.id_usuario = Id_User
 GROUP BY G.id_usuario;
     END$$
+
+DROP PROCEDURE IF EXISTS `INFO_DIARIA`$$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `INFO_DIARIA` (IN `Id_User` INT(11))  BEGIN
+DECLARE FECHA_INI DATETIME;
+SET FECHA_INI= (SELECT DATE(NOW()) - INTERVAL DAY(NOW()) DAY + INTERVAL 1 DAY);
+SELECT 
+      (
+      	SELECT SUM(G2.MONTO)
+        FROM gastos AS G2
+        WHERE G2.id_usuario = G.id_usuario
+        AND G2.fecha BETWEEN FECHA_INI AND CURDATE()
+        AND G2.id_tipo_gasto = 1
+      ) AS GASTOS_ALIMENTACION,
+      (
+      	SELECT SUM(G3.MONTO)
+        FROM gastos AS G3        
+        WHERE G3.id_usuario = G.id_usuario 
+        AND G3.fecha BETWEEN FECHA_INI AND CURDATE()
+        AND G3.id_tipo_gasto = 7
+      ) AS GASTOS_DISTRACCIONES,
+      (
+      	SELECT SUM(G4.MONTO)
+        FROM gastos AS G4        
+        WHERE G4.id_usuario = G.id_usuario 
+        AND G4.fecha BETWEEN FECHA_INI AND CURDATE()
+        AND G4.id_tipo_gasto = 11
+      ) AS GASTOS_HORMIGA     
+      
+FROM GASTOS AS G 
+WHERE G.id_usuario = Id_User
+GROUP BY G.id_usuario;
+END$$
+
+DROP PROCEDURE IF EXISTS `TEST2`$$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `TEST2` (IN `input_data` INT(11), OUT `output_data` VARCHAR(20))  BEGIN
+SELECT descripcion INTO output_data FROM tipo_gasto WHERE id = @input_data;
+END$$
 
 DELIMITER ;
 
@@ -108,7 +151,7 @@ CREATE TABLE IF NOT EXISTS `gastos` (
   PRIMARY KEY (`id`),
   KEY `fk_tipo_g` (`id_tipo_gasto`),
   KEY `fk_user_g` (`id_usuario`)
-) ENGINE=MyISAM AUTO_INCREMENT=7 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=22 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
 --
 -- Volcado de datos para la tabla `gastos`
@@ -118,7 +161,17 @@ INSERT INTO `gastos` (`id`, `monto`, `fecha`, `comprobante`, `observaciones`, `i
 (1, 1000, '2018-11-14', NULL, 'Sándwich Callejero de Desayuno', 1, 1),
 (2, 7500, '2018-11-14', NULL, 'Once con Chaparra', 7, 1),
 (4, 2800, '2018-11-26', NULL, 'Almuerzo Diario', 1, 1),
-(5, 1100, '2018-11-26', NULL, 'Compra de Desayuno Ok Market', 1, 1);
+(5, 1100, '2018-11-26', NULL, 'Compra de Desayuno Ok Market', 1, 1),
+(14, 1000, '2018-12-04', NULL, 'Galletas y Café', 11, 1),
+(13, 2000, '2018-12-04', NULL, 'Almuerzo Diario', 1, 1),
+(12, 8000, '2018-12-04', NULL, 'Lectura con Café', 7, 1),
+(15, 250000, '2018-12-05', NULL, 'Arriendo Mensual', 2, 1),
+(16, 6000, '2018-12-03', NULL, 'Luz Mensual', 3, 1),
+(17, 4000, '2018-12-04', NULL, 'Sin Observaciones', 4, 1),
+(18, 30989, '2018-12-03', NULL, 'Sin Observaciones', 5, 1),
+(19, 10000, '2018-12-03', NULL, 'Sin Observaciones', 6, 1),
+(20, 50000, '2018-12-10', NULL, 'Sin Observaciones', 12, 1),
+(21, 5000, '2018-12-01', NULL, 'carga bip', 11, 1);
 
 -- --------------------------------------------------------
 
@@ -137,7 +190,7 @@ CREATE TABLE IF NOT EXISTS `ingresos` (
   PRIMARY KEY (`id`),
   KEY `fk_tipo_i` (`id_tipo_ingreso`),
   KEY `fk_user_i` (`id_usuario`)
-) ENGINE=MyISAM AUTO_INCREMENT=12 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=15 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
 --
 -- Volcado de datos para la tabla `ingresos`
@@ -145,7 +198,8 @@ CREATE TABLE IF NOT EXISTS `ingresos` (
 
 INSERT INTO `ingresos` (`id`, `monto`, `fecha`, `observaciones`, `id_tipo_ingreso`, `id_usuario`) VALUES
 (9, 750000, '2018-11-14', 'Prueba2', 1, 1),
-(10, 20000, '2018-11-13', 'Sin Observaciones', 2, 1);
+(10, 20000, '2018-11-13', 'Sin Observaciones', 2, 1),
+(14, 765000, '2018-12-01', 'Sin Observaciones', 1, 1);
 
 -- --------------------------------------------------------
 
@@ -158,7 +212,7 @@ CREATE TABLE IF NOT EXISTS `tipo_gasto` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `descripcion` varchar(100) COLLATE utf8_spanish_ci NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=11 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=13 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
 --
 -- Volcado de datos para la tabla `tipo_gasto`
@@ -174,7 +228,9 @@ INSERT INTO `tipo_gasto` (`id`, `descripcion`) VALUES
 (7, 'Distracciones'),
 (8, 'Supermercado'),
 (9, 'Tecnología'),
-(10, 'Donaciones');
+(10, 'Donaciones'),
+(11, 'Otros (Especifícar)'),
+(12, 'Gastos Comunes');
 
 -- --------------------------------------------------------
 
@@ -222,8 +278,8 @@ CREATE TABLE IF NOT EXISTS `users` (
 --
 
 INSERT INTO `users` (`uid`, `username`, `password`, `email`, `name`, `profile_pic`) VALUES
-(1, 'ylopez', '11F72E83FA507C40E7B734E47C072175A39923BB9143EC7E8BD14EFBADAD6EF4', 'yuri_lopez@vtr.net', 'Yuri', ''),
-(2, 'cmontero', '11F72E83FA507C40E7B734E47C072175A39923BB9143EC7E8BD14EFBADAD6EF4', 'cmontero@empresasarmas.cl', 'Cristian', '');
+(1, 'ylopez', '11F72E83FA507C40E7B734E47C072175A39923BB9143EC7E8BD14EFBADAD6EF4', 'yuri_lopez@vtr.net', 'Yuri López', ''),
+(2, 'cmontero', '11F72E83FA507C40E7B734E47C072175A39923BB9143EC7E8BD14EFBADAD6EF4', 'cmontero@empresasarmas.cl', 'Cristian Montero', '');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
